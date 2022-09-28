00f33c039b89e42a60cd6fb2968256f46636a2e9
radhika7171 <aairamahajan@gmail.com>
2022-09-27 23:36:45 -0400

    concepts file

---
+++ day 1/concepts.py
@@ -0,0 +1,30 @@
+
+# # next line
+print("Hello world\nHello world\nHello world")
+#string concatination
+print("Radhika" +" " + "Mahajan")
+#(a("String Concatenation is done with the ")+b(" sign.")) two strings as a and b
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+
+#input function
+print("Hello " + input ("Enter your name ") + "!")
+
+print(len(input("What is your name ?")))
+
+#variable
+name = input ("Enter the name: ")
+length = len(name)
+print(length)
+
+#Swap variables value
+
+a = input("a:")
+b = input("b:")
+
+temp = a
+b = temp
+
+print("a: " + a)
+print("b: " + b)
+
